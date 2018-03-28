268aeb357e120476fe53f3fe2e4872585a74d691
dell <dell@dell-PC>
2018-03-28 23:41:05 +0530

    changes in FindHorseposition-corrected

--- chess project/src/expanion/project1/FindHorseposition.java
+++ chess project/src/expanion/project1/FindHorseposition.java
@@ -4,7 +4,7 @@ public class FindHorseposition {
 	                    /*FUNCTIONAL METHOD TO FIND HORSE POSITION*/
 	String horseposition(byte start_row, byte start_col,byte end_row,byte end_col)
 	{
-	  if(start_row>=0 && start_row<=64 && start_col>=0 && start_col<=64 && end_row>=0 &&end_row<=64 &&end_col>=0 &&end_col<=64 )
+	  if(start_row>=0 && start_row<=7 && start_col>=0 && start_col<=7 && end_row>=0 &&end_row<=7 &&end_col>=0 &&end_col<=7 )
       {
           if(start_row+2== end_row)
           {
